8401bb9f0feb2e2f85dc245f79d95c3465b9395e
肖嘉祥 <xiaojiaxiang@leedarson.com>
2020-09-11 09:04:54 +0800

    模板方法

---
+++ template/模版方法.go
@@ -0,0 +1,61 @@
+package template
+
+import "fmt"
+
+//通用的方法接口
+type Cooker interface {
+	open()
+	fire()
+	cooke()
+	outfire()
+	close()
+}
+
+// 类似于一个抽象类
+type CookMenu struct {
+}
+
+func (CookMenu) open() {
+	fmt.Println("打开开关")
+}
+
+func (CookMenu) fire() {
+	fmt.Println("开火")
+}
+
+// 做菜，交给具体的子类实现
+func (CookMenu) cooke() {
+}
+
+func (CookMenu) outfire() {
+	fmt.Println("关火")
+}
+
+func (CookMenu) close() {
+	fmt.Println("关闭开关")
+}
+
+// 封装具体步骤
+func doCook(cook Cooker) {
+	cook.open()
+	cook.fire()
+	cook.cooke()
+	cook.outfire()
+	cook.close()
+}
+
+type XiHongShi struct {
+	CookMenu
+}
+
+func (*XiHongShi) cooke() {
+	fmt.Println("做西红柿")
+}
+
+type ChaoJiDan struct {
+	CookMenu
+}
+
+func (ChaoJiDan) cooke() {
+	fmt.Println("做炒鸡蛋")
+}
